984148da77757943258259276f811f561e3da9dd
John McBride <john@papercompute.com>
2026-08-03 15:53:48 -0400

    ✨ feat: Initial commit
    Signed-off-by: John McBride <john@papercompute.com>

---
+++ flake.nix
@@ -0,0 +1,43 @@
+{
+  description = "Search cassette - Development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    dagger.url = "github:dagger/nix";
+    dagger.inputs.nixpkgs.follows = "nixpkgs";
+    paper-skills.url = "github:papercomputeco/skills";
+    paper-skills.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, dagger, paper-skills }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        skills = paper-skills.lib;
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            # Go toolchain
+            pkgs.go_1_26
+            pkgs.gotools
+            pkgs.sqlc
+
+            # Build tools
+            pkgs.gnumake
+            dagger.packages.${system}.dagger
+          ];
+
+          shellHook = ''
+            echo "Search cassette development environment"
+            echo ""
+            echo "Go version: $(go version)"
+            echo ""
+            echo "Available make targets:"
+            make help
+          '';
+        };
+      }
+    );
+}
